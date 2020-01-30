6f3b9fc589977ee072d55ca6edf6f33bbc8357a0
Julian Nonino <noninojulian@gmail.com>
2020-01-30 12:55:57 +0000

    Updating variables

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 # ---------------------------------------------------------------------------------------------------------------------
 # ECS Cluster
 # ---------------------------------------------------------------------------------------------------------------------
-module "ecs-cluster" {
+module ecs-cluster {
   source  = "cn-terraform/ecs-cluster/aws"
   version = "1.0.2"
   name    = var.name_preffix
@@ -22,11 +22,11 @@ module "ecs-cluster" {
 # ---------------------------------------------------------------------------------------------------------------------
 module "ecs-fargate-task-definition" {
   source       = "cn-terraform/ecs-fargate-task-definition/aws"
-  version      = "1.0.5"
+  version      = "1.0.8"
   name_preffix = var.name_preffix
   profile      = var.profile
   region       = var.region
-  command = var.command
+  command                      = var.command
   container_name               = var.container_name
   container_image              = var.container_image
   container_port               = var.container_port
@@ -39,6 +39,7 @@ module "ecs-fargate-task-definition" {
   entrypoint                   = var.entrypoint
   environment                  = var.environment
   essential                    = var.essential
+  firelens_configuration       = var.firelens_configuration
   healthcheck                  = var.healthcheck
   links                        = var.links
   log_configuration            = var.log_configuration
@@ -48,42 +49,43 @@ module "ecs-fargate-task-definition" {
   secrets                      = var.secrets
   start_timeout                = var.start_timeout
   stop_timeout                 = var.stop_timeout
+  system_controls              = var.system_controls
   ulimits                      = var.ulimits
   user                         = var.user
   volumes_from                 = var.volumes_from
   working_directory            = var.working_directory
   placement_constraints        = var.placement_constraints_task_definition
   proxy_configuration          = var.proxy_configuration
-  system_controls              = var.system_controls
-  firelens_configuration       = var.firelens_configuration
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ECS Service
 # ---------------------------------------------------------------------------------------------------------------------
-module "ecs-fargate-service" { 
+module "ecs-fargate-service" {
   source              = "cn-terraform/ecs-fargate-service/aws"
-  version             = "1.0.3"
+  version             = "1.0.6"
   name_preffix        = var.name_preffix
   profile             = var.profile
   region              = var.region
   vpc_id              = var.vpc_id
   subnets             = var.private_subnets_ids
-  ecs_cluster_name    = module.ecs-cluster.aws_ecs_cluster_cluster_name
-  ecs_cluster_arn     = module.ecs-cluster.aws_ecs_cluster_cluster_arn
-  task_definition_arn                = module.ecs-fargate-task-definition.aws_ecs_task_definition_td_arn
-  container_name                     = var.container_name
-  container_port                     = module.ecs-fargate-task-definition.container_port
-  desired_count                      = var.desired_count
-  platform_version                   = var.platform_version
+
+  ecs_cluster_name      = module.ecs-cluster.aws_ecs_cluster_cluster_name
+  ecs_cluster_arn       = module.ecs-cluster.aws_ecs_cluster_cluster_arn
+  task_definition_arn   = module.ecs-fargate-task-definition.aws_ecs_task_definition_td_arn
+  container_name        = module.ecs-fargate-task-definition.container_name
+  container_port        = module.ecs-fargate-task-definition.container_port
+  desired_count         = var.desired_count
+  platform_version      = var.platform_version
+  propagate_tags        = var.propagate_tags
+  service_registries    = var.service_registries
+  security_groups       = var.security_groups
+  assign_public_ip      = var.assign_public_ip
+  placement_constraints = var.placement_constraints_ecs_service
+  lb_health_check_path  = var.lb_health_check_path
   deployment_maximum_percent         = var.deployment_maximum_percent
   deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent
   enable_ecs_managed_tags            = var.enable_ecs_managed_tags
-  propagate_tags                     = var.propagate_tags
   ordered_placement_strategy         = var.ordered_placement_strategy
   health_check_grace_period_seconds  = var.health_check_grace_period_seconds
-  placement_constraints              = var.placement_constraints_ecs_service
-  service_registries                 = var.service_registries
-  security_groups                    = var.security_groups
-  assign_public_ip                   = var.assign_public_ip
 }
--- variables.tf
+++ variables.tf
@@ -45,17 +45,14 @@ locals {
 variable "vpc_id" {
   description = "ID of the VPC"
 }
-
 variable "availability_zones" {
   type        = list
   description = "List of Availability Zones"
 }
-
 variable "public_subnets_ids" {
   type        = list
   description = "List of Public Subnets IDs"
 }
-
 variable "private_subnets_ids" {
   type        = list
   description = "List of Private Subnets IDs"
@@ -64,78 +61,69 @@ variable "private_subnets_ids" {
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS ECS Container Definition Variables
 # ---------------------------------------------------------------------------------------------------------------------
+variable "command" {
+  type        = list(string)
+  description = "(Optional) The command that is passed to the container"
+  default     = null
+}
 variable "container_port" {
   description = "Port on which the container is listening"
 }
-
 variable "container_image" {
   description = "The image used to start the container."
 }
-
 variable "container_name" {
   description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
 }
-
-variable "command" {
-  type        = list
-  description = "(Optional) The command that is passed to the container"
-  default     = [""]
-}
-
 variable "container_cpu" {
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
   description = "(Optional) The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
-  default     = 1024 # 1 vCPU 
+  default     = 1024 # 1 vCPU
 }
-
 variable "container_depends_on" {
-  type        = list
+  type        = list(string)
   description = "(Optional) The dependencies defined for container startup and shutdown. A container can contain multiple dependencies. When a dependency is defined for container startup, for container shutdown it is reversed"
-  default     = []
+  default     = null
 }
-
 variable "container_memory" {
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
   description = "(Optional) The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed. This field is optional for Fargate launch type and the total amount of container_memory of all containers in a task will need to be lower than the task memory value"
   default     = 8192 # 8 GB
 }
-
 variable "container_memory_reservation" {
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
   description = "(Optional) The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit"
   default     = 2048 # 2 GB
 }
-
 variable "dns_servers" {
-  type        = list
-  description = "(Optional) Container DNS servers. This is a list of strings specifying the IP addresses of the DNS servers."
-  default     = []
+  type        = list(string)
+  description = "(Optional) Container DNS servers. This is a list of strings specifying the IP addresses of the DNS servers"
+  default     = null
 }
-
 variable "docker_labels" {
   type        = map(string)
-  description = "The configuration options to send to the `docker_labels`"
+  description = "(Optional) The configuration options to send to the `docker_labels`"
   default     = null
 }
 
 variable "entrypoint" {
-  type        = list
+  type        = list(string)
   description = "(Optional) The entry point that is passed to the container"
-  default     = [""]
+  default     = null
 }
-
 variable "environment" {
-  type        = list
-  description = "(Optional) The environment variables to pass to the container. This is a list of maps. Each map should contain `name` and `value`"
-  default     = []
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  description = "(Optional) The environment variables to pass to the container. This is a list of maps"
+  default     = null
 }
-
 variable "essential" {
-  type        = string
+  type        = bool
   description = "(Optional) Determines whether all other containers in a task are stopped, if this container fails or stops for any reason. Due to how Terraform type casts booleans in json it is required to double quote this value"
-  default     = "true"
+  default     = true
 }
-
 # https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_FirelensConfiguration.html
 variable "firelens_configuration" {
   type = object({
@@ -145,7 +133,6 @@ variable "firelens_configuration" {
   description = "(Optional) The FireLens configuration for the container. This is used to specify and configure a log router for container logs. For more details, see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_FirelensConfiguration.html"
   default     = null
 }
-
 # https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HealthCheck.html
 variable "healthcheck" {
   type = object({
@@ -158,13 +145,11 @@ variable "healthcheck" {
   description = "(Optional) A map containing command (string), timeout, interval (duration in seconds), retries (1-10, number of times to retry before marking container unhealthy), and startPeriod (0-300, optional grace period to wait, in seconds, before failed healthchecks count toward retries)"
   default     = null
 }
-
 variable "links" {
-  type        = list
-  description = "(Optional) List of container names this container can communicate with without port mappings."
-  default     = []
+  type        = list(string)
+  description = "(Optional) List of container names this container can communicate with without port mappings"
+  default     = null
 }
-
 # https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html
 variable "log_configuration" {
   type = object({
@@ -178,68 +163,72 @@ variable "log_configuration" {
   description = "(Optional) Log configuration options to send to a custom log driver for the container. For more details, see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html"
   default     = null
 }
-
 variable "mount_points" {
-  type        = list
+  type = list(object({
+    containerPath = string
+    sourceVolume  = string
+  }))
   description = "(Optional) Container mount points. This is a list of maps, where each map should contain a `containerPath` and `sourceVolume`"
-  default     = []
+  default     = null
 }
-
 variable "readonly_root_filesystem" {
-  type        = string
+  type        = bool
   description = "(Optional) Determines whether a container is given read-only access to its root filesystem. Due to how Terraform type casts booleans in json it is required to double quote this value"
-  default     = "false"
+  default     = false
 }
-
 variable "repository_credentials" {
-  type        = map
+  type        = map(string)
   description = "(Optional) Container repository credentials; required when using a private repo.  This map currently supports a single key; \"credentialsParameter\", which should be the ARN of a Secrets Manager's secret holding the credentials"
-  default     = {}
+  default     = null
 }
-
 variable "secrets" {
-  type        = list
+  type = list(object({
+    name      = string
+    valueFrom = string
+  }))
   description = "(Optional) The secrets to pass to the container. This is a list of maps"
-  default     = []
+  default     = null
 }
-
 variable "start_timeout" {
   description = "(Optional) Time duration (in seconds) to wait before giving up on resolving dependencies for a container."
   default     = 30
 }
-
 variable "stop_timeout" {
+  type        = number
   description = "(Optional) Timeout in seconds between sending SIGTERM and SIGKILL to container"
   default     = 30
 }
-
 variable "system_controls" {
   type        = list(map(string))
   description = "(Optional) A list of namespaced kernel parameters to set in the container, mapping to the --sysctl option to docker run. This is a list of maps: { namespace = \"\", value = \"\"}"
   default     = null
 }
-
 variable "ulimits" {
-  type        = list
+  type = list(object({
+    name      = string
+    hardLimit = number
+    softLimit = number
+  }))
   description = "(Optional) Container ulimit settings. This is a list of maps, where each map should contain \"name\", \"hardLimit\" and \"softLimit\""
-  default     = []
+  default     = null
 }
-
 variable "user" {
+  type        = string
   description = "(Optional) The user to run as inside the container. Can be any of these formats: user, user:group, uid, uid:gid, user:gid, uid:group"
-  default     = ""
+  default     = null
 }
-
 variable "volumes_from" {
-  type        = list
-  description = "(Optional) A list of VolumesFrom maps which contain \"sourceContainer\" (name of the container that has the volumes to mount) and \"readOnly\" (whether the container can write to the volume)."
-  default     = []
+  type = list(object({
+    sourceContainer = string
+    readOnly        = bool
+  }))
+  description = "(Optional) A list of VolumesFrom maps which contain \"sourceContainer\" (name of the container that has the volumes to mount) and \"readOnly\" (whether the container can write to the volume)"
+  default     = null
 }
-
 variable "working_directory" {
   type        = string
   description = "(Optional) The working directory to run commands inside the container"
-  default     = ""
+  default     = null
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -260,88 +249,66 @@ variable "proxy_configuration" {
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS ECS SERVICE
 # ---------------------------------------------------------------------------------------------------------------------
-variable "task_definition_arn" {
-  description = "(Required) The full ARN of the task definition that you want to run in your service."
-}
-
-variable "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
-}
-
-variable "ecs_cluster_arn" {
-  description = "ARN of an ECS cluster"
-}
-
-variable "subnets" {
-  description = "The subnets associated with the task or service."
-  type        = list
-}
-
 variable "desired_count" {
   description = "(Optional) The number of instances of the task definition to place and keep running. Defaults to 1."
   type        = number
   default     = 1
 }
-
 variable "platform_version" {
   description = "(Optional) The platform version on which to run your service. Defaults to LATEST. More information about Fargate platform versions can be found in the AWS ECS User Guide."
   default     = "LATEST"
 }
-
 variable "deployment_maximum_percent" {
   description = "(Optional) The upper limit (as a percentage of the service's desiredCount) of the number of running tasks that can be running in a service during a deployment."
   type        = number
   default     = 200
 }
-
 variable "deployment_minimum_healthy_percent" {
   description = "(Optional) The lower limit (as a percentage of the service's desiredCount) of the number of running tasks that must remain running and healthy in a service during a deployment."
   type        = number
   default     = 100
 }
-
 variable "enable_ecs_managed_tags" {
-  description = "(Optional) Specifies whether to enable Amazon ECS managed tags for the tasks within the service."
-  default     = ""
+  description = "(Optional) Specifies whether to enable Amazon ECS managed tags for the tasks within the service.Valid values are true or false. Default true."
+  type        = bool
+  default     = false
 }
-
 variable "propagate_tags" {
   description = "(Optional) Specifies whether to propagate the tags from the task definition or the service to the tasks. The valid values are SERVICE and TASK_DEFINITION. Default to SERVICE"
   default     = "SERVICE"
 }
-
 variable "ordered_placement_strategy" {
   description = "(Optional) Service level strategy rules that are taken into consideration during task placement. List from top to bottom in order of precedence. The maximum number of ordered_placement_strategy blocks is 5. This is a list of maps where each map should contain \"id\" and \"field\""
   type        = list
   default     = []
 }
-
 variable "health_check_grace_period_seconds" {
   description = "(Optional) Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 2147483647. Only valid for services configured to use load balancers."
   type        = number
   default     = 0
 }
-
 variable "placement_constraints_ecs_service" {
   type        = list
   description = "(Optional) rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. This is a list of maps, where each map should contain \"type\" and \"expression\""
   default     = []
 }
-
 variable "service_registries" {
   description = "(Optional) The service discovery registries for the service. The maximum number of service_registries blocks is 1. This is a map that should contain the following fields \"registry_arn\", \"port\", \"container_port\" and \"container_name\""
   type        = map
   default     = {}
 }
-
 variable "security_groups" {
   description = "(Optional) The security groups associated with the task or service. If you do not specify a security group, the default security group for the VPC is used."
   type        = list
   default     = []
 }
-
 variable "assign_public_ip" {
   description = "(Optional) Assign a public IP address to the ENI (Fargate launch type only). Valid values are true or false. Default false."
   type        = bool
   default     = false
+}
+variable "lb_health_check_path" {
+  description = "(Optional) Health check path for the Load Balancer"
+  type        = string
+  default     = "/"
 }
