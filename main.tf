3e6a41fa406ef7a72ae1a2b2a83aefbc19e83915
Julian Nonino <noninojulian@gmail.com>
2020-11-26 18:45:32 +0000

    Updating task definition module

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module ecs-cluster {
 #------------------------------------------------------------------------------
 module "td" {
   source  = "cn-terraform/ecs-fargate-task-definition/aws"
-  version = "1.0.16"
+  version = "1.0.17"
   # source  = "../terraform-aws-ecs-fargate-task-definition"
 
   name_prefix                  = var.name_prefix
