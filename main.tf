588e848746e6670a61a688b809e8901ad9557ee3
Julian Nonino <noninojulian@gmail.com>
2021-02-25 07:44:16 -0300

    Update module dependencies

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "td" {
 #------------------------------------------------------------------------------
 module "ecs-fargate-service" {
   source  = "cn-terraform/ecs-fargate-service/aws"
-  version = "2.0.11"
+  version = "2.0.13"
   # source  = "../terraform-aws-ecs-fargate-service"
 
   name_prefix = var.name_prefix
