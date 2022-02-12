35bcb2b5467102177c3c65aa734d5f0ddaf4fa10
Julian Nonino <noninojulian@gmail.com>
2022-02-12 20:58:24 +0000

    Fix pipeline

--- examples/test/mock_provider.tf
+++ examples/test/mock_provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.1"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "us-east-1"
   skip_credentials_validation = true
