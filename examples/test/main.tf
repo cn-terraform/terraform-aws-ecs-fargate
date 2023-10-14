94a156c81e3e5c1ec7fe6b317900dbea1d437b1f
JaredDarling <46824759+JaredDarling@users.noreply.github.com>
2023-10-13 17:05:03 -0700

    Test (#7)
    * String any (#3)
    
    * Add deployment_controller (#1)
    
    * Add deployment_controller
    
    Add support for setting deployment_controller of service
    
    * Change type string -> any
    
    * Change type string -> any
    
    * use test branch ref
    
    * Formatting
    
    * Adds support for additional load balancer feature & using force_new_deployment flag of ECS.
    
    * Adds the ability to configure additional containers (sidecars)
    
    * Formatting
    
    * Update to latest module versions
    
    * Sets default deployment_controller to fix test
    
    ---------
    
    Co-authored-by: Jared Darling <jdarling@aimconsulting.com>

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -1,10 +1,48 @@
+locals {
+  public_subnet_ids  = [for s in module.base-network.public_subnets : s.id]
+  private_subnet_ids = [for s in module.base-network.private_subnets : s.id]
+}
+
 module "base-network" {
-  source                                      = "cn-terraform/networking/aws"
-  name_prefix                                 = "test-networking"
-  vpc_cidr_block                              = "192.168.0.0/16"
-  availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+  source     = "cn-terraform/networking/aws"
+  cidr_block = "192.168.0.0/16"
+
+  vpc_additional_tags = {
+    vpc_tag1 = "tag1",
+    vpc_tag2 = "tag2",
+  }
+
+  public_subnets = {
+    first_public_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.0.0/19"
+    }
+    second_public_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.32.0/19"
+    }
+  }
+
+  public_subnets_additional_tags = {
+    public_subnet_tag1 = "tag1",
+    public_subnet_tag2 = "tag2",
+  }
+
+  private_subnets = {
+    first_private_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.128.0/19"
+    }
+    second_private_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.160.0/19"
+    }
+  }
+
+  private_subnets_additional_tags = {
+    private_subnet_tag1 = "tag1",
+    private_subnet_tag2 = "tag2",
+  }
 }
 
 module "test" {
@@ -13,6 +51,6 @@ module "test" {
   vpc_id              = module.base-network.vpc_id
   container_image     = "ubuntu"
   container_name      = "test"
-  public_subnets_ids  = module.base-network.public_subnets_ids
-  private_subnets_ids = module.base-network.private_subnets_ids
+  public_subnets_ids  = local.public_subnet_ids
+  private_subnets_ids = local.private_subnet_ids
 }
